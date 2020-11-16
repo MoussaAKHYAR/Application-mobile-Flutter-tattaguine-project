00aaf147d95a997a7275c606dc37726229ac3da0
MoussaAKHYAR <amrane93.mb@gmail.com>
2020-11-16 16:31:38 +0000

    gestion de la liste des pharmacies

--- lib/liste-pharmacie.dart
+++ lib/liste-pharmacie.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 class PharmaciePage extends StatefulWidget {
   @override
   _PharmaciePageState createState() => _PharmaciePageState();
@@ -12,7 +13,18 @@ class _PharmaciePageState extends State<PharmaciePage> {
     return Scaffold(
       appBar: AppBar(title: Text("Pharmacies"),),
       body: Center(
-        child: Text("liste des pharamcies .... "),
+        child: this.listVilles==null?CircularProgressIndicator():
+            ListView.builder(
+                itemCount: (this.listVilles==null)?0:this.listVilles.length,
+                itemBuilder: (context,index){
+                  return Card(
+                    color: Colors.cyanAccent,
+                    child: RaisedButton(
+                        child: Text(this.listVilles[index]['nom']),
+                    ),
+                  );
+                }
+            )
       ),
     );
   }
@@ -24,13 +36,12 @@ class _PharmaciePageState extends State<PharmaciePage> {
   }
 
   void loadPharmacie() {
-    String url = "http://192.168.7.11:8081/pharmacies";
+    String url = "http://192.168.137.132:8081/pharmacies";
     http.get(url)
         .then((resp){
           setState(() {
-
+            this.listVilles=json.decode(resp.body)['_embedded']['pharmacies'];
           });
-          
     }).catchError((err){
       print(err);
     });
